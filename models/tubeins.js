37568add6b2831eb7b280cb19fc4205ffff353b8
kamal shadi <kamal.shadi85@gmail.com>
2018-01-27 21:15:29 -0500

    tubeins

--- models/tubeins.js
+++ models/tubeins.js
@@ -2,10 +2,35 @@ var mongoose = require("mongoose"),
   Schema = mongoose.Schema;
 const connection = require('../model_app').connection;
 
+const tubeinSchema = new Schema({
+    "tubeinType": {type:String, default:'user'},
+    "userID": { type: Schema.Types.ObjectId, ref: 'User' },
+    "fullname": String,
+    "description": String,
+    "geo": {
+      "continent": String,
+      "city": String,
+      "country": String,
+      "state": String,
+      "latitude": Number,
+      "longitude": Number
+    },
+    "duration": {type:Number, default:60},
+    "screenSizeAvailable": {type:String, default:"one-fifth.png"},
+    "tubeinTime": { type: Date, default: Date.now },
+    "tubeinStatusChangeTime": { type: Date, default: Date.now },
+    "revenue": {type:Number, default:0},
+    "startTime": {type:Number, default:0},
+    "track": {type:Number, default:1},
+    "userTubeinStatus": {type:String, default:"pending"},
+    "filled":{type:Boolean, default:false},
+    "icon": {type:String, default:"female-user.jpeg"}
+});
+
 var tubeinsSchema = new Schema({
   "presetFlag" : {type:Boolean, default:true},
   "global": {
-    "tubeID": { type: Schema.Types.ObjectId, ref: 'Event' },
+    "tubeID": { type: Schema.Types.ObjectId, ref: 'Tube' },
     "tubeDuration": {type:Number, default:0},
     "tubeinSessionDuration": {type:Number, default:0},
     "startTime": {type:Number, default: Date.now },
@@ -26,108 +51,12 @@ var tubeinsSchema = new Schema({
     }
   ],
   "tubeins": [
-    [
-      {
-        "tubeinType": {type:String, default:'user'},
-        "userID": { type: Schema.Types.ObjectId, ref: 'User' },
-        "fullname": String,
-        "description": String,
-        "geo": {
-          "continent": String,
-          "city": String,
-          "country": String,
-          "state": String,
-          "latitude": Number,
-          "longitude": Number
-        },
-        "duration": Number,
-        "screenSizeAvailable": String,
-        "tubeinTime": { type: Date, default: Date.now },
-        "tubeinStatusChangeTime": { type: Date, default: Date.now },
-        "revenue": {type:Number, default:0},
-        "startTime": {type:Number, default:0},
-        "track": {type:Number, default:1},
-        "userTubeinStatus": {type:String, default:"pending"}
-      }
-    ],
-    [
-    {
-      "tubeinType": {type:String, default:'user'},
-      "userID": { type: Schema.Types.ObjectId, ref: 'User' },
-      "fullname": String,
-      "description": String,
-      "geo": {
-        "continent": String,
-        "city": String,
-        "country": String,
-        "state": String,
-        "latitude": Number,
-        "longitude": Number
-      },
-      "duration": Number,
-      "screenSizeAvailable": String,
-      "tubeinTime": { type: Date, default: Date.now },
-      "tubeinStatusChangeTime": { type: Date, default: Date.now },
-      "revenue": {type:Number, default:0},
-      "startTime": {type:Number, default:0},
-      "track": {type:Number, default:1},
-      "userTubeinStatus": {type:String, default:"pending"}
-    }
-    ]
+    [tubeinSchema],
+    [tubeinSchema],
   ],
   "acceptedTubeins": [
-    [
-      {
-        "tubeinType": {type:String, default:'user'},
-        "userID": { type: Schema.Types.ObjectId, ref: 'User' },
-        "fullname": String,
-        "description": String,
-        "geo": {
-          "continent": String,
-          "city": String,
-          "country": String,
-          "state": String,
-          "latitude": Number,
-          "longitude": Number
-        },
-        "duration": Number,
-        "screenSizeAvailable": String,
-        "tubeinTime": { type: Date, default: Date.now },
-        "tubeinStatusChangeTime": { type: Date, default: Date.now },
-        "revenue": {type:Number, default:0},
-        "startTime": {type:Number, default:0},
-        "track": {type:Number, default:1},
-        "userTubeinStatus": {type:String, default:"pending"},
-        "filled":{type:Boolean, default:false},
-        "icon": {type:String, default:"female-user.jpeg"}
-      }
-    ],
-    [
-      {
-        "tubeinType": {type:String, default:'user'},
-        "userID": { type: Schema.Types.ObjectId, ref: 'User' },
-        "fullname": String,
-        "description": String,
-        "geo": {
-          "continent": String,
-          "city": String,
-          "country": String,
-          "state": String,
-          "latitude": Number,
-          "longitude": Number
-        },
-        "duration": Number,
-        "screenSizeAvailable": String,
-        "tubeinTime": { type: Date, default: Date.now },
-        "tubeinStatusChangeTime": { type: Date, default: Date.now },
-        "revenue": {type:Number, default:0},
-        "startTime": {type:Number, default:0},
-        "track": {type:Number, default:1},
-        "userTubeinStatus": {type:String, default:"pending"},
-        "filled":{type:Boolean, default:false},
-        "icon": {type:String, default:"female-user.jpeg"}
-      }
-    ]
+    [tubeinSchema],
+    [tubeinSchema]
   ]
 },{ collection: 'tubeins' });
 
@@ -189,5 +118,11 @@ class TubeinsModel {
 
 }
 
+class TubeinModel {
+
+}
+
+const Tubein = tubeinSchema.loadClass(TubeinModel);
 const Tubeins = tubeinsSchema.loadClass(TubeinsModel);
-module.exports = connection.model("Tubeins", Tubeins);
+module.exports.Tubeins = mongoose.model("Tubeins", Tubeins);
+module.exports.Tubein = mongoose.model("Tubein", Tubein);
